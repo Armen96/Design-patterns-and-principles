6240b5b0e7b553cc5b121f31ad93fd069dcb3ed7
Armen96 <barsegyan96armen@gmail.com>
2019-10-12 13:55:51 +0400

    Java Proxy pattern

---
+++ java/patterns/src/ProxyPattern.java
@@ -0,0 +1,32 @@
+public class ProxyPattern {
+    public static void main(String[] args) {
+        ICarDrive reno = new CarXProxy(new CarX());
+        reno.drive();
+    }
+}
+
+interface ICarDrive {
+    void drive();
+}
+
+class CarXProxy implements ICarDrive {
+    CarX carX;
+
+    CarXProxy(CarX carX) {
+        this.carX = carX;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Proxy job: adding additional header");
+        carX.drive();
+    }
+}
+
+class CarX implements ICarDrive {
+
+    @Override
+    public void drive() {
+        System.out.println("Car is driving!!!");
+    }
+}
